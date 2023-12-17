44964fce5e77bbe094e87becc3ec9ef9efcda271
Насыров Максим <doctor1119@yandex.ru>
2023-12-17 18:04:46 +0300

    Исправление 500 ошибки из-за блокировки IP

--- app/parser/tass_parser.py
+++ app/parser/tass_parser.py
@@ -1,13 +1,14 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 import asyncio
 import aiohttp
+import random
 from app.services.async_parser import scrape
 from app.services.yahofin import get_currency_history
 
 
 def date_generator(start_date, end_date, step_hours=24):
     current_date = start_date
-    while current_date <= end_date:
+    while current_date <= end_date - timedelta(hours=step_hours):
         yield f"https://tass.ru/tbp/api/v1/search?limit=20&offset=0&lang=ru&types=news&rubrics=mezhdunarodnaya" \
               f"-panorama&rubrics=ekonomika&sort=-es_updated_dt&from_date={current_date.isoformat()}&to" \
               f"_date={(current_date + timedelta(hours=step_hours)).isoformat()}"
@@ -15,37 +16,39 @@ def date_generator(start_date, end_date, step_hours=24):
 
 
 async def get_response(start, end, max_concurrent_requests=5):
+    """ Асинхронный запрос к API для получения данных. """
     history_curs = get_currency_history(start_date=start, end_date=end)
     start_date = datetime.strptime(start, '%Y-%m-%d')
     end_date = datetime.strptime(end, '%Y-%m-%d')
-    result = []
+
     semaphore = asyncio.Semaphore(max_concurrent_requests)
-    async with aiohttp.ClientSession() as session:
-        tasks = [safe_fetch(session, url, semaphore) for url in list(date_generator(start_date, end_date))]
-        responses = await asyncio.gather(*tasks)
+    connector = aiohttp.TCPConnector(limit_per_host=5)
+    timeout = aiohttp.ClientTimeout(total=180)
+    async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
+        fetch_tasks = [safe_fetch(session, url, semaphore) for url in date_generator(start_date, end_date)]
+        responses = await asyncio.gather(*fetch_tasks, return_exceptions=True)
         parse_tasks = []
         article_metadata = []
-        for articles in responses:
-            for article in articles.get('result'):
-                url = f"https://tass.ru{article.get('url')}"
-                published_dt = datetime.fromisoformat(article.get('published_dt'))
-                timestamp = int(
-                    published_dt.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc).timestamp())
-                parse_tasks.append(await scrape(session, url))
-                tags = [tag.get('name') for tag in article.get('tags')]
-                rubrics = [rubric.get('name') for rubric in article.get('rubrics')]
+        result = []
+        for response in responses:
+            for article in response.get('result', []):
+                url = f"https://tass.ru{article.get('url', '')}"
+                published_dt = datetime.fromisoformat(article.get('published_dt')).strftime('%Y-%m-%d')
+                parse_tasks.append(scrape(session, url))
+
+                tags = [tag.get('name') for tag in article.get('tags', [])]
+                rubrics = [rubric.get('name') for rubric in article.get('rubrics', [])]
                 article_metadata.append({
                     'article_id': article.get('id'),
                     'title': article.get('title'),
                     'url': url,
-                    'published_dt': published_dt.strftime('%Y-%m-%d'),
-                    'published_timestamp': timestamp,
+                    'published_dt': published_dt,
                     'tags': tags + rubrics,
                     'meta_description': article.get('meta_description'),
-                    'currency_curs': history_curs.get(f"{timestamp}")
+                    'currency_curs': history_curs.get(f"{published_dt}")
                 })
-                print(published_dt)
-        parsed_texts = await asyncio.gather(*parse_tasks)
+
+        parsed_texts = await asyncio.gather(*parse_tasks, return_exceptions=True)
 
         for metadata, text in zip(article_metadata, parsed_texts):
             metadata['text'] = text
@@ -57,12 +60,14 @@ async def get_response(start, end, max_concurrent_requests=5):
 # TODO: Обработка ошибок сервера
 async def safe_fetch(session, url, semaphore):
     try:
-        async with semaphore, session.get(url) as response:
-            if response.status == 200:
-                return await response.json()
-            else:
-                # Обработка неудачных статусов HTTP
-                return None
+        async with semaphore:
+            await asyncio.sleep(random.uniform(0.5, 1.5))
+            async with session.get(url) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    print(f"HTTP Error {response.status} for URL: {url}")
+                    return None
     except Exception as e:
         # Логирование или обработка исключений
         return None
