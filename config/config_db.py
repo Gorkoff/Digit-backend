d5789c3d8b1248c09485b6d2e69ffc74230712f4
mrnasyrov <mrnasyrov@hsworld.ru>
2024-02-20 17:55:17 +0300

    Первая миграция

--- config/config_db.py
+++ config/config_db.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+
+load_dotenv(dotenv_path)
 
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
