3f511f0f34ef8321c2ece79de4e8e145e34efdfa
bryzgalova <eibryzgalova@mai.education>
2024-02-27 11:24:52 +0300

    Добавила сервис предпроцессинг

--- app/api.py
+++ app/api.py
@@ -17,5 +17,7 @@ async def get_articles(body: Body):
     timeout = aiohttp.ClientTimeout(total=TIMEOUT)
     history_curs = get_currency_history(start_date=body.start_date, end_date=body.end_date)
     async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
-        return await get_tass_articles(body.start_date, body.end_date, session, history_curs) + await get_rbk_articles(
+        data_articles = await get_tass_articles(body.start_date, body.end_date, session, history_curs) + await get_rbk_articles(
             body.start_date, body.end_date, session, history_curs)
+        predproc(data_articles)
+        return data_articles
