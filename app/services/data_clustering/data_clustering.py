36fcef9641f8d946658a57b3064840af0a44c6d3
Artem <artemgorkov040@yandex.ru>
2024-05-02 22:38:53 +0300

    Добавлены наброски data_clustering.py,

--- app/services/data_clustering/data_clustering.py
+++ app/services/data_clustering/data_clustering.py
@@ -1,3 +1,21 @@
 import pandas as pd
 from app.services.data_collection import collect_articles
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 
+
+def clusterize_articles(preprocessed_articles):
+    # Кластеризация статей на основе TF-IDF
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(preprocessed_articles["text"])
+
+    # Используем KMeans для кластеризации
+    kmeans = KMeans(n_clusters=2, random_state=42)  # Два кластера для примера
+    preprocessed_articles["cluster_id"] = kmeans.fit_predict(tfidf_matrix)
+
+    return preprocessed_articles
+
+
+def convert_to_json(clustered_articles):
+    # Преобразуем DataFrame в JSON-массив
+    return clustered_articles.to_dict(orient="records")
