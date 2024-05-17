405df93e4864b7963e27f350cc1e700d575d0dac
Artem <artemgorkov040@yandex.ru>
2024-05-18 01:02:38 +0300

    Пофиксил получение данных из окружения

--- app/services/data_base/data_base.py
+++ app/services/data_base/data_base.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-import os
+from app.services.data_base.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
-DATABASE_URL = f"postgresql+asyncpg://{os.environ['DB_USER']}:{os.environ['DB_PASS']}@{os.environ['DB_HOST']}/{os.environ['DB_NAME']}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Создаем асинхронный движок SQLAlchemy
 engine = create_async_engine(DATABASE_URL, echo=True)
