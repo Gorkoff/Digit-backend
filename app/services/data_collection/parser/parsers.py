bb9f882d55bade95c5a5ee0d2990d80da8b07ef9
Artem <artemgorkov040@yandex.ru>
2024-05-18 00:53:57 +0300

    Добавил parsers.py

---
+++ app/services/data_collection/parser/parsers.py
@@ -0,0 +1,20 @@
+from sqlalchemy.future import select
+from sqlalchemy import insert
+from app.models import article
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def fetch_articles_from_db(session: AsyncSession, start_date: str, end_date: str):
+    result = await session.execute(
+        select(article).where(article.c.published_dt >= start_date).where(article.c.published_dt <= end_date)
+    )
+    return result.fetchall()
+
+
+async def save_articles_to_db(session: AsyncSession, articles):
+    async with session.begin():
+        await session.execute(
+            insert(article),
+            articles
+        )
+    await session.commit()
