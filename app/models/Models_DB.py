b1ee4bef37420c1e23d2c53c9b1479398044e123
Artem <artemgorkov040@yandex.ru>
2024-06-11 01:51:07 +0300

    Добавлена кластеризированная индексация

--- app/models/Models_DB.py
+++ app/models/Models_DB.py
@@ -1,6 +1,13 @@
-from sqlalchemy import MetaData, Integer, String, Table, Column, VARCHAR, FLOAT, TIMESTAMP, DATETIME
+from sqlalchemy import MetaData, Integer, String, Table, Column, VARCHAR, FLOAT, TIMESTAMP, Index
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.sql import text
+import asyncio
+from app.services.data_base.data_base import database_engine
+from app.services.data_base.config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
 metadata = MetaData()
+
+database_engine = create_async_engine(f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
 
 article = Table(
     "article",
@@ -12,3 +19,19 @@ article = Table(
     Column("currency_curs", FLOAT),
     Column("text", String),
 )
+
+# Кластеризованный индекс (для PostgreSQL нужно будет использовать `postgresql_using='cluster'` в create_engine)
+index = Index('ix_published_dt', article.c.published_dt)
+
+
+# Асинхронная функция для создания таблицы и индекса
+async def create_tables_and_cluster():
+    async with database_engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+
+    # Выполнение команды CLUSTER отдельно
+    async with database_engine.connect() as conn:
+        await conn.execute(text("CLUSTER article USING ix_published_dt"))
+
+# Вызов асинхронной функции
+asyncio.run(create_tables_and_cluster())
--- app/services/data_base/data_base.py
+++ app/services/data_base/data_base.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from app.services.data_base.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-engine = create_async_engine(DATABASE_URL, future=True)
-AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+database_engine = create_async_engine(DATABASE_URL, future=True)
+AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=database_engine, class_=AsyncSession)
 
 
 async def get_session() -> AsyncSession:
